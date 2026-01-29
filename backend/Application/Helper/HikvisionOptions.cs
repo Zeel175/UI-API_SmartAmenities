1ec127060d82e72b3026053bbdff63c1073119a0
Zeel175 <zeel@skywardtechno.us>
2026-01-29 15:19:23 +0530

    Add timeout for Hikvision face image in OTP verify

--- backend/Application/Helper/HikvisionOptions.cs
+++ backend/Application/Helper/HikvisionOptions.cs
@@ -11,5 +11,6 @@ namespace Application.Helper
         public string BaseUrl { get; set; } = "";
         public string Username { get; set; } = "";
         public string Password { get; set; } = "";
+        public int FaceImageTimeoutSeconds { get; set; } = 3;
     }
 }
